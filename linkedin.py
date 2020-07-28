d7d831cb6b290194d4369a843ca504b0516e31c5
okazkayasi <mkazkayasi3@gatech.edu>
2020-07-28 19:49:44 +0300

    pipeline is set. linkedin done

--- linkedin.py
+++ linkedin.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 import csv
 
 
-def open_browser():
+def open_browser(visible=False):
 
     config = configparser.ConfigParser()
     config.read('file.ini')
@@ -18,7 +18,10 @@ def open_browser():
     pw = config.get('auth', 'password')
 
     option = webdriver.ChromeOptions()
+    # option.add_argument("user-data-dir=C:/Users/okazkayasi/AppData/Local/Google/Chrome/User Data/") #Path to your chrome profile
     option.add_argument("--start-maximized")
+    if not visible:
+        option.add_argument("--headless")
 
 #     option.add_argument("--incognito")
     # option.add_argument("--headless")
@@ -36,25 +39,35 @@ def open_browser():
 
 
 def get_active_names(br):
+    time.sleep(2)
+
+    pip = br.find_element(By.CLASS_NAME, "search-results__list")\
+        .find_elements_by_xpath("//span[text()='Status is reachable']")
     people = br.find_element(By.CLASS_NAME, "search-results__list")\
-        .find_elements_by_class_name('presence-indicator--is-reachable')
+        .find_elements_by_xpath("//span[text()='Status is online']")
+    del people[-1]
+    online = len(people)
+    reach = len(pip)
+    people.extend(pip)
     names = []
     for p in people:
-        name = (p.find_element_by_xpath('../../../../../../..')
+        try:
+            name = (p.find_element_by_xpath('../../../../../../../..')
                 .find_element(By.CLASS_NAME, "search-result__info")
                 .find_element(By.CLASS_NAME, "actor-name").text)
-        names.append(name)
-    return names
+            names.append(name)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+            return get_active_names(br)
+    return names, online, reach
 
 
 def go_next_page(br):
-    time.sleep(1)
-    active = br.find_element_by_class_name(
-        'artdeco-pagination__button--next').is_enabled()
-    if not active:
-        return False
+
+
     br.find_element_by_class_name('artdeco-pagination__button--next').click()
-    time.sleep(1)
+    time.sleep(4)
     return True
 
 
@@ -66,26 +79,70 @@ def go_down(br, n=1):
         html.send_keys(Keys.HOME)
 
 
-if __name__ == "__main__":
-    br = open_browser()
+
+def fun(br):
+
     time.sleep(3)
     br.get('https://www.linkedin.com/search/results/people/?facetNetwork=%5B%22F%22%5D&origin=MEMBER_PROFILE_CANNED_SEARCH')
-    all_people = []
+    time.sleep(3)
+    try:
+        if len(br.find_elements_by_class_name('msg-overlay-list-bubble--is-minimized')) == 0:
+            br.find_element_by_class_name('msg-overlay-bubble-header').click()
+    except:
+        print('no message box')
 
+    all_people = []
+    online = 0
+    reach = 0
+    check_people = ['asd']
     active = True
+    page = 1
     while True:
-        all_people.extend(get_active_names(br))
-        print(len(all_people))
         go_down(br)
-        if not go_next_page(br):
+        page_people, page_online, page_reach = get_active_names(br)
+        print(page_people)
+        
+        online += page_online
+        reach += page_reach
+
+        if page_people == check_people:
             break
 
+        # go to next page           
+        all_people.extend(page_people)
+        print('online: {} and reachable {} at page {}'.format(online, reach, page))
+        page += 1
+        check_people = page_people
+        go_next_page(br)
+
+
     fields = [datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-           len(all_people)]
+        online, reach]
     fields.extend(all_people)
     
-    with open('names.csv', 'a', newline='\n') as f:
+    with open('names.csv', 'a', newline='\n', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
 
-    br.close()
+    config = configparser.ConfigParser()
+    config.read('file.ini')
+    sleep_time = int(config.get('auth', 'sleep_minutes'))
+    print('sleep for {} minutes'.format(sleep_time))
+    time.sleep(sleep_time*60)
+
+
+if __name__ == "__main__":
+    br = open_browser()
+    run_time = 0
+    while True:
+        try:
+            fun(br)
+        except Exception as e:
+            if run_time == 0:  # means it didn't work even one time.
+                br = open_browser(visible=True)
+                try:
+                    fun(br)
+                except Exception as e:
+                    print('You have 3 minutes to solve the puzzle', e)
+                    time.sleep(180)
+        run_time += 1
--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 import datetime
 import ftplib
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
+import pandas as pd
+import gc
 
 d1 = datetime.date(2019, 1, 1)
 d2 = datetime.date(2020, 6, 30)
@@ -23,11 +23,21 @@ ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
 ftp.encoding = "utf-8"
 ftp.cwd('ais_data')
 
-for filename in filenames[:2]:
-    filename = 'aisdk_20200101.csv'
+for filename in filenames[:5]:
+    print(filename)
     with open(filename, "wb") as file:
         # use FTP's RETR command to download the file
         ftp.retrbinary(f"RETR {filename}", file.write)
 
-    table = pv.read_csv(filename)
-    pq.write_table(table, filename.replace('csv', 'parquet'))
+    print('ftp done')
+    df = pd.read_csv(filename)
+    print('read df')
+    df.to_parquet(filename.replace('csv', 'parquet.gzip'), compression='gzip')
+    del df
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+    print('del df and file')
+    gc.collect()
+    df = pd.DataFrame()
